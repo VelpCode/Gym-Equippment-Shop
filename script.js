8fc1c269505fb64f0b36219cde025ab83dcbca4b
Aman Velpula <velpmail@gmail.com>
2023-03-14 19:13:43 -0400

    Added dark theme feature

--- script.js
+++ script.js
@@ -191,3 +191,20 @@ function filterProduct(value) {
   window.onload = () => {
     filterProduct("all");
   };
+
+  const options = {
+    bottom: '64px', // default: '32px'
+    right: 'unset', // default: '32px'
+    left: '32px', // default: 'unset'
+    time: '0.5s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: false, // default: true,
+    label: '🚀', // default: ''
+    autoMatchOsTheme: true // default: true
+  }
+  
+  const darkmode = new Darkmode(options);
+  darkmode.showWidget();
