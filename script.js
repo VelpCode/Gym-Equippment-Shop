a40bb97433cefa8f2f4a19ea29844b7a3bf7ca66
Aman Velpula <velpmail@gmail.com>
2023-03-15 19:20:16 -0400

    Index wrapper finalzing velpstore

--- script.js
+++ script.js
@@ -77,20 +77,6 @@ let products = {
 };
 
 
-const initGetDocumentation = () => {
-  const menuSidebar = document.querySelector("#menu-sidebar")
-  const appSidebar = document.querySelector("#app-sidebar")
-  menuSidebar.addEventListener("click", () => {
-      appSidebar.classList.toggle("show")
-  })
-
-
-}
-
-
-initGetDocumentation();
-
-
 function toggleModal() {
   document.body.classList.toggle("modal--open");
 }
@@ -220,3 +206,17 @@ function filterProduct(value) {
   
   const darkmode = new Darkmode(options);
   darkmode.showWidget();
+
+
+  const initGetDocumentation = () => {
+    const menuSidebar = document.querySelector("#menu-sidebar")
+    const appSidebar = document.querySelector("#app-sidebar")
+    menuSidebar.addEventListener("click", () => {
+    appSidebar.classList.toggle("show")
+    })
+      
+      
+    }
+      
+      
+    initGetDocumentation();
